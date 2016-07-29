1b90c4244178c74d3a391055c4755b9f3b905e7f
GionatanG <gionatangallo@gmail.com>
2016-07-29 11:01:10 +0200

    libgd and libconfig
    Added libraries libgd/libconfig

--- collageMaker.c
+++ collageMaker.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /* Bring in gd library functions */
-#include "libgd-2.2.3/src/gd.h"
+#include "lib/libgd-2.2.3/src/gd.h"
 /*include our header files*/
 #include "collageMaker.h"
 #include "ioUtils.h"
