7c3879be566d3513079ae78fb8ea0b4e391990be
ChiaraC <chiara.caiazza@gmail.com>
2016-08-02 14:53:16 +0200

    vips library used

--- ioUtils.h
+++ ioUtils.h
@@ -1,8 +1,10 @@
+#include <stdlib.h>
+
 #include "collageMaker.h"
-#include "lib/libgd-2.2.3/src/gd.h"
+
+
 
 
 int retrieveInput (struct layout* , int );
-int createOutputImage(gdImagePtr);
 int scanInputValue (int, char**, struct layout* , int);
 
