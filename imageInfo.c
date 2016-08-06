4401993307549e8f492ae9d98b949598b4abec7c
mtroscia <mtroscia@msn.com>
2016-08-06 19:48:32 +0200

    added functions
    -) added width/height ratio function for the array of images
    -) added "best match" function

--- imageInfo.c
+++ imageInfo.c
@@ -10,13 +10,11 @@ get_height(const VipsImage *image) {
 	return vips_image_get_height(image);
 }
 
-/*horizontal image resolution in pixels per millimeter*/
 double
 get_horiz_resol(const VipsImage *image) {
 	return vips_image_get_xres(image);
 }
 
-/*vertical image resolution in pixels per millimeter*/
 double 
 get_vert_resol(const VipsImage *image) {
 	return vips_image_get_yres(image);
@@ -39,7 +37,7 @@ int
 min_height(struct collageMakerImage *list, int num_elem){
 	int i, i_app = 0;
 	int min = get_height(list[0].image);
-	for (i=0; i<num_elem; i++){
+	for (i=1; i<num_elem; i++){
 		if (get_height(list[i].image) < min){
 			min = get_height(list[i].image);
 			i_app = i;
@@ -52,13 +50,64 @@ int
 min_width(struct collageMakerImage *list, int num_elem){
 	int i, i_app = 0;
 	int min = get_width(list[0].image);
-	for (i=0; i<num_elem; i++){
+	for (i=1; i<num_elem; i++){
 		if (get_width(list[i].image) < min){
 			min = get_width(list[i].image);
 			i_app = i;
 		}
 	}
 	return i_app;
+}
+
+double*
+im_width_over_height(struct collageMakerImage *list, int num_elem){
+	int i;
+	double* ret = (double*)malloc(sizeof(double)*num_elem);
+	for (i=0; i<num_elem; i++)
+		ret[i] = (double)get_width(list[i].image) / (double)get_height(list[i].image);
+	return ret;
+}
+	
+int*
+find_best_match(double* images_ratio, double* frames_ratio, int num_elem){
+	int i, j;
+	int remaining = num_elem;
+	int image_index, frame_index;
+	double min;
+	int* ret = (int*)malloc(sizeof(int)*num_elem);
+	double** matrix = (double**)malloc(sizeof(double*)*num_elem);
+	for (i=0; i<num_elem; i++){		
+		matrix[i] = (double*)malloc(sizeof(double)*num_elem);
+	}
+	for (i=0; i<num_elem; i++){	//for every image
+		for (j=0; j<num_elem; j++){	//for every frame
+			if (frames_ratio[j] > images_ratio[i])
+				matrix[i][j] = frames_ratio[j]-images_ratio[i];
+			else
+				matrix[i][j] = images_ratio[i]-frames_ratio[j];
+		}
+	}
+	while (remaining!=0){
+		image_index = 0;
+		frame_index = 0;
+		min = matrix[0][0];
+		for (i=0; i<num_elem; i++){
+			for (j=0; j<num_elem; j++){
+				if (matrix[i][j] < min){					
+					min = matrix[i][j];
+					image_index = i;
+					frame_index = j;
+				}
+			}
+		}
+		ret[image_index] = frame_index;
+		remaining--;
+		for (i=0; i<num_elem; i++){
+			matrix[image_index][i] = 10.0;
+			matrix[i][frame_index] = 10.0;
+		}	
+	}
+	return ret;
 }
 
 
