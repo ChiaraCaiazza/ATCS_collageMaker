4016059c55a0d154319ddd0e151d1c1a64248a65
mtroscia <mtroscia@msn.com>
2016-08-12 18:39:45 +0200

    update files

--- collageMaker.c
+++ collageMaker.c
@@ -4,23 +4,6 @@
 #include "layout.h"
 #include "imageUtils.h"
 
-
-/*
-comando per debian :
-		gcc -I/usr/include/glib-2.0 -I/usr/lib/glib-2.0 -lconfig -Wall collageMaker.c ioUtils.c layout.c imageInfo.c `pkg-config --cflags glib-2.0` `pkg-config --libs glib-2.0` `pkg-config vips --cflags --libs` -o collageMaker
-
-		
-
-comando per mac:
-		?
-
-exec:
-		./collageMaker [-n num (default=2)] [-t string (default=null)] [-o string (default=null)]
-
-*/
-
-
-
 int main(int argc, char **argv) {
 	int ret;
 
